81b13ef879a6b675de6d3a5894a6ca0ddfdc6e0e
Ken <73114932+kennardmah@users.noreply.github.com>
2023-11-13 23:02:27 +0000

    first draft of map

--- bikes.r
+++ bikes.r
@@ -1,21 +1,22 @@
-install.packages('httr')
+# Install and load packages
+if (!require("leaflet")) install.packages("leaflet")
+if (!require("ggplot2")) install.packages("ggplot2")
+if (!require("data.table")) install.packages("data.table")
 
-library(httr)
+library(leaflet)
+library(ggplot2)
+library(data.table)
 
-base_url <- "https://data.boston.gov/api/3/action/datastore_search"
-params <- list(
-    resource_id = 'e4bfe397-6bfc-49c5-9367-c879fac7401d'
-    limit = 5
-)
+# Load dataframe for accidents
+df <- fread("accident-data/vision-zero-crash.csv")
 
-response <- GET(url = base_url, query = params)
+# Filter to cyclists
+bike_df <- df[mode_type == "bike"]
 
-# Check if the request was successful
-if (http_status(response)$category == "success") {
-  data <- content(response, as = "text")  # Extract content from the response object
-  data <- fromJSON(data)  # Parse the JSON content
-  
-  print(data$result$records)  # Print the results
-} else {
-  print(paste("Failed to retrieve data:", http_status(response)$reason))
-}
+# Create a leaflet map with clustering
+map <- leaflet(bike_df) %>%
+  addTiles() %>%
+  addMarkers(~long, ~lat, popup = ~as.character(`dispatch_ts`), clusterOptions = markerClusterOptions())
+
+# Display the map
+map
