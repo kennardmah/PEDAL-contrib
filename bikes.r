f44817f0a5ba0f801760f12d02d118659ec0fb05
Ken <73114932+kennardmah@users.noreply.github.com>
2023-11-16 22:23:39 +0000

    heatmap

--- bikes.r
+++ bikes.r
@@ -1,6 +1,5 @@
 # # Install and load packages
-# install.packages(c("leaflet", "ggplot2",
-#   "data.table", "dbscan"))
+# install.packages(c("leaflet", "ggplot2", "data.table"))
 
 library(leaflet)
 library(ggplot2)
@@ -12,10 +11,28 @@ df <- fread("accident-data/vision-zero-crash.csv")
 # Filter to cyclists
 bike_df <- df[mode_type == "bike"]
 
-# Create a leaflet map with clustering
+# Add the isDark column
+bike_df[, isDark := ifelse(as.integer(format(dispatch_ts, "%H")) >= 15 | as.integer(format(dispatch_ts, "%H")) < 6, 1, 0)]
+dark_df <- bike_df[isDark == 1]
+light_df <- bike_df[isDark == 0]
+
+# Create a leaflet map
 map <- leaflet(bike_df) %>%
-  addTiles() %>%
-  addMarkers(~long, ~lat, popup = ~as.character(`dispatch_ts`), clusterOptions = markerClusterOptions())
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+
+  # Add markers for dark conditions
+  addCircleMarkers(data = bike_df[isDark == 1], ~long, ~lat, 
+                   popup = ~as.character(dispatch_ts), 
+                   group = 'Dark Conditions', color = 'blue', fillOpacity = 0.1, weight = 0) %>%
+
+  # Add markers for light conditions
+  addCircleMarkers(data = bike_df[isDark == 0], ~long, ~lat, 
+                   popup = ~as.character(dispatch_ts), 
+                   group = 'Light Conditions', color = 'red', fillOpacity = 0.1, weight = 0) %>%
+
+  # Add layer control
+  addLayersControl(overlayGroups = c('Dark Conditions', 'Light Conditions'),
+                   options = layersControlOptions(collapsed = FALSE))
 
 # Display the map
 map
