b77289872433c49c65aa30439882d98b86fd0a85
Ken <73114932+kennardmah@users.noreply.github.com>
2023-11-17 12:00:50 +0000

    implement shiny

--- bike.py
+++
@@ -1,6 +0,0 @@
-import json
-import urllib.request
-url = 'https://data.boston.gov/api/3/action/datastore_search?resource_id=e4bfe397-6bfc-49c5-9367-c879fac7401d&limit=5&q=title:jones'  
-fileobj = urllib.request.urlopen(url)
-response_dict = json.loads(fileobj.read())
-print(response_dict)
--- combined_map.r
+++ combined_map.r
@@ -1,26 +1,57 @@
 # Load required libraries
+library(shiny)
 library(leaflet)
 library(sf)
 library(dplyr)
 library(data.table)
 library(ggplot2)
 
-# Load and prepare bike accident data
-df <- fread("dataframe/vision-zero-crash.csv")
-bike_df <- df[df$mode_type == "bike"]
-bike_df$isDark <- ifelse(as.integer(format(bike_df$dispatch_ts, "%H")) >= 15 | as.integer(format(bike_df$dispatch_ts, "%H")) < 6, 1, 0)
+# Define UI
+ui <- fluidPage(
+  # Reference "www/styles.css"
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  # Title
+  div(class = "title-panel",
+      tags$h1("CycleCompanion: Interactive Analysis of Cycling for Informed Decision Making")
+  ),
+  # Sidebar
+  div(class = "leaflet-sidebar",
+      sidebarPanel(
+        HTML("<h4>Description</h4>"),
+        textOutput("description")
+      )
+  ),
+  # Underlying Map
+  leafletOutput("map")
+)
 
-# Load and filter infrastructure data
-bike_lanes_geojson <- sf::read_sf("dataframe/Existing_Bike_Network_2023.geojson")
-filtered_bike_lanes <- bike_lanes_geojson %>% filter(!(ExisFacil %in% c("WALK", "PED")))
+# Define server logic
+server <- function(input, output) {
 
-# Create a combined map
-combined_map <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap tiles
-  # Add infrastructure layers
-  addPolylines(data = filtered_bike_lanes, color = "Green", weight = 3, opacity = 0.7, group = ~ExisFacil) %>%
-  # Add accident markers for dark conditions
-  addCircleMarkers(data = bike_df, ~long, ~lat, popup = ~as.character(dispatch_ts), group = 'Dark Conditions', color = 'red', fillOpacity = 0.2, weight = 0, radius = 3)
+  # Provide descriptive text
+  output$description <- renderText({
+    "This data product visualizes bike accidents in relation to the existing bike infrastructure. Red markers indicate accidents that occurred in dark conditions, offering insights into potential areas for safety improvements."
+  })
 
-# Display the combined map
-combined_map
+  # Load and prepare bike accident data
+  df <- fread("dataframe/vision-zero-crash.csv")
+  bike_df <- df[df$mode_type == "bike"]
+  bike_df$isDark <- ifelse(as.integer(format(bike_df$dispatch_ts, "%H")) >= 15 | as.integer(format(bike_df$dispatch_ts, "%H")) < 6, 1, 0)
+
+  # Load and filter infrastructure data
+  bike_lanes_geojson <- sf::read_sf("dataframe/Existing_Bike_Network_2023.geojson")
+  filtered_bike_lanes <- bike_lanes_geojson %>% filter(!(ExisFacil %in% c("WALK", "PED")))
+
+  # Create and render the map
+  output$map <- renderLeaflet({
+    leaflet(filtered_bike_lanes) %>%
+      addTiles() %>%
+      addPolylines(data = filtered_bike_lanes, color = "Green", weight = 3, opacity = 0.7, group = ~ExisFacil) %>%
+      addCircleMarkers(data = bike_df, ~long, ~lat, popup = ~as.character(dispatch_ts), color = 'red', fillOpacity = 0.2, weight = 0, radius = 3)
+  })
+}
+
+# Create Shiny app
+shinyApp(ui, server)
