5825a131b3e9398ac6bf78d425666e6467260b43
Ken <73114932+kennardmah@users.noreply.github.com>
2023-12-09 14:26:40 +0000

    coordinates

--- coordinates_csv.r
+++ coordinates_csv.r
@@ -1,19 +1,44 @@
-# upload library
-library(data.table)
+# Install and load the sf package
+library(sf)
 
-# Import the CSV file using fread
-pollution_data <- fread("dataframe/boston-air-quality.csv")
-# Remove the columns
-pollution_data[, (c("pm10", "o3", "so2", "co")) := NULL]
+file_path <- "dataframe/pre-processed/
+             Boston_Neighborhood_Boundaries_approximated_by_2020
+             _Census_Block_Groups.geojson"
 
-# Convert the 'date' column to Date type
-pollution_data[, date := as.Date(date, format = "%Y/%m/%d")]
+# Read the GeoJSON file
+geo_data <- st_read(file_path)
 
-# Sort the data table by 'date'
-pollution_data <- pollution_data[order(date)]
-# Filter out dates before 2016
-pollution_data <- pollution_data[date >= as.Date("2017-01-01")
-    & date <= as.Date("2023-11-30")]
+# Extract only the name and geometry
+processed_data <- geo_data[c("blockgr2020_ctr_neighb_name", "geometry")]
+# st_write(processed_data, "dataframe/post-processed/name-coordinates.geojson", driver = "GeoJSON")
 
-# Save the sorted data table to a new CSV file
-fwrite(pollution_data, "dataframe/sorted_boston_air_quality.csv")
+# Calculate centroids of the geometries
+centroids <- st_centroid(processed_data$geometry)
+# Extract longitude and latitude from centroids
+lon <- st_coordinates(centroids)[,1]
+lat <- st_coordinates(centroids)[,2]
+# Create a data frame with location names and their centroid coordinates
+centroid_data <- data.frame(
+  blockgr2020_ctr_neighb_name = processed_data$blockgr2020_ctr_neighb_name,
+  longitude = lon,
+  latitude = lat
+)
+
+# Save CSV file
+write.csv(centroid_data, "dataframe/post-processed/name-to-coordinates.csv", row.names = FALSE)
+
+
+### TEST BY MAPPING ON LEAFLET ###
+
+# # Calculate centroids and add them to the geo_data
+# processed_data$longitude <- st_coordinates(st_centroid(processed_data$geometry))[,1]
+# processed_data$latitude <- st_coordinates(st_centroid(processed_data$geometry))[,2]
+
+# # Create a leaflet map with a minimal base map
+# leaflet(processed_data) %>%
+#   addProviderTiles(providers$CartoDB.Positron, options = providerTileOptions(noWrap = TRUE)) %>%
+#   addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+#               opacity = 1.0, fillOpacity = 0.6,
+#               highlight = highlightOptions(weight = 3, color = "#666", fillOpacity = 0.7)) %>%
+#   addCircleMarkers(lng = ~longitude, lat = ~latitude, 
+#                    radius = 5, color = "#007bff", stroke = FALSE, fillOpacity = 1)
